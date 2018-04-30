eef7752f77388394af63f5bf365241456b6d4f05
Chanon Roy <chanonr@surveymonkey.com>
2018-04-30 14:09:39 -0400

    finished mvp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-import BookList from './components/BookList'
-import AddBook from './components/AddBook'
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 // Apollo Client Setup
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-})
+    uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="main">
-          <h1> Reading List </h1>
-          <BookList />
-          <AddBook />
-        </div>
-      </ApolloProvider>
-    )
-  }
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <div className="main">
+                    <h1> Reading List </h1>
+                    <BookList />
+                    <AddBook />
+                </div>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -9,6 +9,25 @@ const getBooksQuery = gql`
   }
 `
 
+const getBookQuery = gql`
+  query GetBook($id: ID) {
+        book(id: $id) {
+            id
+            name
+            genre
+            author {
+                id
+                name
+                age
+                books {
+                    name
+                    id
+                }
+            }
+        }
+    }
+`
+
 
 const getAuthorsQuery = gql`
     {
@@ -19,4 +38,13 @@ const getAuthorsQuery = gql`
     }
 `
 
-export { getAuthorsQuery, getBooksQuery }
+const addBookMutation = gql`
+    mutation($name: String!, $genre: String!, $authorId: ID!){
+        addBook(name: $name, genre: $genre, authorId: $authorId){
+            name
+            id
+        }
+    }
+`;
+
+export { getAuthorsQuery, getBooksQuery, getBookQuery, addBookMutation }
